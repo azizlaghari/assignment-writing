206c84a2dc83a2157f72f19a79a95d1a5f1d7dd1
azizlaghari <azizlaghari9@gmail.com>
2023-05-09 21:00:39 +0500

    Add header

--- assignment-writing/src/components/Header.jsx
+++ assignment-writing/src/components/Header.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
-  return (
-    <>
-    <h1>hello</h1>
-    </>
+    return (
+        <>
+            <header className="transparent-header">
+                <div className="logo">
+                    <span>Assignment Writing</span>
+                </div>
+                <nav className="navigation">
+                    <ul>
+                        <li><a href="#">Link 1</a></li>
+                        <li><a href="#">Link 2</a></li>
+                        <li><a href="#">Link 3</a></li>
+                        <li><a href="#">Link 4</a></li>
+                        <li><a href="#">Link 5</a></li>
+                        <div className="buttons">
+                            <a href="#" className="button button-primary">Button 1</a>
+                            <a href="#" className="button button-secondary">Button 2</a>
+                        </div>
+                    </ul>
+                </nav>
+            </header>
+
+        </>
     )
 }
 
