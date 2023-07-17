0fd8c28048ffd5ddc052bfb3c09556d9b75e4511
azizlaghari <azizlaghari9@gmail.com>
2023-07-17 10:51:21 +0500

    add some necessory comments on variables

--- rtk-multiple-ra/src/App.jsx
+++ rtk-multiple-ra/src/App.jsx
@@ -1,21 +1,35 @@
+// Import necessary dependencies from React and Redux.
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+
+// Import the 'increment' and 'decrement' actions from the 'counterSlice'.
 import { increment, decrement } from './redux/counterSlice';
+
+// Import the 'setName' and 'setEmail' actions from the 'userSlice'.
 import { setName, setEmail } from './redux/userSlice';
 
+// Functional component 'App'.
 function App() {
+  // Select the 'counter' value from the Redux store using 'useSelector' hook.
   const counter = useSelector((state) => state.counter.value);
+
+  // Select the 'user' object from the Redux store using 'useSelector' hook.
   const user = useSelector((state) => state.user);
+
+  // Get the 'dispatch' function using 'useDispatch' hook to dispatch actions.
   const dispatch = useDispatch();
 
+  // Handler function to dispatch 'setName' action on name input change.
   const handleNameChange = (event) => {
     dispatch(setName(event.target.value));
   };
 
+  // Handler function to dispatch 'setEmail' action on email input change.
   const handleEmailChange = (event) => {
     dispatch(setEmail(event.target.value));
   };
 
+  // Return the JSX for the App component.
   return (
     <div>
       <h1>Counter: {counter}</h1>
@@ -43,4 +57,5 @@ function App() {
   );
 }
 
+// Export the App component as the default export.
 export default App;
