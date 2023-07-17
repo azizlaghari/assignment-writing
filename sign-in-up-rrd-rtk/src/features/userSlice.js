3d48d40ad6aaf48f01c3ce02890311ac2a1a6de6
azizlaghari <azizlaghari9@gmail.com>
2023-07-17 10:29:48 +0500

    add some comments on variables

--- sign-in-up-rrd-rtk/src/features/userSlice.js
+++ sign-in-up-rrd-rtk/src/features/userSlice.js
@@ -1,23 +1,31 @@
 // features/userSlice.js
-import { createSlice } from '@reduxjs/toolkit';
+// Import the 'createSlice' function from Redux Toolkit.
+import { createSlice } from "@reduxjs/toolkit";
 
+// Define the initial state of the 'user' slice with a 'user' property set to null.
 const initialState = {
-    user: null,
+  user: null,
 };
 
+// Create a Redux slice named 'user' with 'setUser' and 'clearUser' reducers.
 const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        setUser: (state, action) => {
-            state.user = action.payload;
-        },
-        clearUser: (state) => {
-            state.user = null;
-        },
+  name: "user",
+  initialState,
+  // Reducer functions to handle 'setUser' and 'clearUser' actions.
+  reducers: {
+    // Reducer to set the 'user' state to the value provided in the action payload.
+    setUser: (state, action) => {
+      state.user = action.payload;
     },
+    // Reducer to set the 'user' state to null, effectively clearing the user data.
+    clearUser: (state) => {
+      state.user = null;
+    },
+  },
 });
 
+// Extract the 'setUser' and 'clearUser' actions from the 'userSlice' object.
 export const { setUser, clearUser } = userSlice.actions;
 
+// Export the reducer function for the 'user' slice to be used in the Redux store.
 export default userSlice.reducer;
