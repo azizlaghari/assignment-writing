d31bbba3a570f620931b62dc29ec49effd27588c
azizlaghari <azizlaghari9@gmail.com>
2023-07-18 10:42:55 +0500

    add comments on variables

--- registration-form-rtk/src/redux/registrationSlice.js
+++ registration-form-rtk/src/redux/registrationSlice.js
@@ -1,24 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
+// Import the 'createSlice' function from Redux Toolkit.
+import { createSlice } from "@reduxjs/toolkit";
 
+// Create a Redux slice named 'registration' with initial state containing empty 'name', 'email', and 'password' properties.
 const registrationSlice = createSlice({
-    name: 'registration',
-    initialState: {
-        name: '',
-        email: '',
-        password: '',
+  name: "registration",
+  initialState: {
+    name: "",
+    email: "",
+    password: "",
+  },
+  // Reducer functions to handle 'setName', 'setEmail', and 'setPassword' actions.
+  reducers: {
+    // Reducer to set the 'name' state to the value provided in the action payload.
+    setName: (state, action) => {
+      state.name = action.payload;
     },
-    reducers: {
-        setName: (state, action) => {
-            state.name = action.payload;
-        },
-        setEmail: (state, action) => {
-            state.email = action.payload;
-        },
-        setPassword: (state, action) => {
-            state.password = action.payload;
-        },
+    // Reducer to set the 'email' state to the value provided in the action payload.
+    setEmail: (state, action) => {
+      state.email = action.payload;
     },
+    // Reducer to set the 'password' state to the value provided in the action payload.
+    setPassword: (state, action) => {
+      state.password = action.payload;
+    },
+  },
 });
 
+// Extract the 'setName', 'setEmail', and 'setPassword' actions from the 'registrationSlice' object.
 export const { setName, setEmail, setPassword } = registrationSlice.actions;
+
+// Export the reducer function for the 'registration' slice to be used in the Redux store.
 export default registrationSlice.reducer;
